7521013d8da27245a1197d089e4afe2345bf2ff8
tskhu <tskhu@TREVOR>
2025-08-19 13:09:34 +0200

    Add project files.

---
+++ QuickMath/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace QuickMath
+{
+    partial class CfrmQuickMath
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpMST = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.lblTimeRemaining = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.grpMST.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpMST
+            // 
+            this.grpMST.Controls.Add(this.btnStart);
+            this.grpMST.Controls.Add(this.lblResult);
+            this.grpMST.Controls.Add(this.txtAnswer);
+            this.grpMST.Controls.Add(this.lblAnswer);
+            this.grpMST.Controls.Add(this.btnAnswer);
+            this.grpMST.Controls.Add(this.lblTimeRemaining);
+            this.grpMST.Controls.Add(this.lblScore);
+            this.grpMST.Controls.Add(this.lblQuestion);
+            this.grpMST.Location = new System.Drawing.Point(12, 12);
+            this.grpMST.Name = "grpMST";
+            this.grpMST.Size = new System.Drawing.Size(378, 201);
+            this.grpMST.TabIndex = 10;
+            this.grpMST.TabStop = false;
+            this.grpMST.Text = "Math Speed Test";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(270, 156);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 39);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(177, 49);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(195, 55);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "Correct!";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswer.Location = new System.Drawing.Point(118, 81);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(40, 38);
+            this.txtAnswer.TabIndex = 4;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(6, 81);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(120, 31);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "Answer: ";
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(8, 115);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(104, 39);
+            this.btnAnswer.TabIndex = 6;
+            this.btnAnswer.Text = "Answer";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // lblTimeRemaining
+            // 
+            this.lblTimeRemaining.AutoSize = true;
+            this.lblTimeRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeRemaining.Location = new System.Drawing.Point(6, 16);
+            this.lblTimeRemaining.Name = "lblTimeRemaining";
+            this.lblTimeRemaining.Size = new System.Drawing.Size(245, 33);
+            this.lblTimeRemaining.TabIndex = 1;
+            this.lblTimeRemaining.Text = "Time Remaining: ";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(6, 156);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(123, 33);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(6, 49);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(138, 31);
+            this.lblQuestion.TabIndex = 3;
+            this.lblQuestion.Text = "Question: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CfrmQuickMath
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 224);
+            this.Controls.Add(this.grpMST);
+            this.Name = "CfrmQuickMath";
+            this.Text = "CfrmQuickMath";
+            this.grpMST.ResumeLayout(false);
+            this.grpMST.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMST;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.Label lblTimeRemaining;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ QuickMath/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickMath
+{
+    
+    public partial class CfrmQuickMath : Form
+    {
+        //Declare global variables
+        Random Single = new Random();
+        int correctAnswer;
+        int score = 0;
+        int timeLeft = 25;
+        
+
+        public CfrmQuickMath()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Display remaining time, start timer, and generate a question
+            int score = 0;
+            timeLeft = 25;
+            lblScore.Text = "Score: 0";
+            lblTimeRemaining.Text = "Time Remaining: " + timer1;
+            lblResult.Text = "";
+            timer1.Start();
+            GenerateQuestion();
+        }
+
+    
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            //save answer of the user into an int variable variable
+            int answer = int.Parse(txtAnswer.Text);
+            //If answer is the same as the correct answer, increment score by one and change color of text to green
+            if(answer == correctAnswer)
+            {
+                score++;
+                lblScore.Text = string.Format("{0}", score);
+                lblResult.ForeColor = Color.Green;
+                lblResult.Text = "Correct!\n+2 Sec";
+                timeLeft += 2;
+
+            }
+            //If answer is not the same as the correct answer, decrement score by five and change color of text to red
+            else
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Wrong!\n-5 Sec";
+                timeLeft -= 5;
+            }
+            //clear the answer and generate a question
+            txtAnswer.Clear();
+            GenerateQuestion();
+        }
+        private void GenerateQuestion()
+        {
+            //Variables with random nnumbers from 1 to 10
+            int num1 = Single.Next(1, 11);
+            int num2 = Single.Next(1, 11);
+            int RandomOperation = Single.Next(1, 4);
+            
+            switch (RandomOperation)
+            {
+                //Operations for every case
+                case 1:
+                    correctAnswer = num1 + num2;
+                    lblQuestion.Text = string.Format("{0} + {1} = ?", num1, num2);
+                    break;
+                case 2:
+                    correctAnswer = num1 - num2;
+                    lblQuestion.Text = string.Format("{0} - {1} = ?", num1, num2);
+                    break;
+                case 3:
+                    correctAnswer = num1 * num2;
+                    lblQuestion.Text = string.Format("{0} x {1} = ?", num1, num2);
+                    break;
+                default:
+                    
+                    break;
+
+            }
+         
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Decrementing the score by 1 everytime the time clicks
+            timeLeft--;
+            lblTimeRemaining.Text = string.Format("{0}", timeLeft);
+            //if time is less or equal to zero, stop the timer and display a message box with the score
+            if (timeLeft <= 0)
+            {
+                timer1.Stop();
+                lblTimeRemaining.Text = "Time Remaining: 0";
+                MessageBox.Show(string.Format("Time’s Up!Final Score: {0}\nGame Over",score));
+            }
+        }
+    }
+}
